fc8e9f00c4ce70f08165bfbcdb630784ad5192fc
Khalidxx <FXTRyio@gmail.com>
2018-09-28 18:38:18 +0800

    SRTN debugging

--- BurstTimeComparator.java
+++ BurstTimeComparator.java
@@ -5,8 +5,9 @@ public class BurstTimeComparator implements Comparator<Process>
     @Override
     public int compare(Process p1, Process p2)
     {
-        
-        return p2.burstTime - p1.getBurstTime();
-    }
-    
+        if(p1.getBurstTime() == p2.getBurstTime()){
+            return p1.getPriority() - p2.getPriority();
+        }
+        return p1.getBurstTime() - p2.getBurstTime();
+    } 
 }
